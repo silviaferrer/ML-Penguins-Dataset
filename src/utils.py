dcc891469584c2039310b2cb1dc2bb5e3497f80b
Ignacio Lloret <ig.lloret.l@gmail.com>
2024-04-30 16:20:11 +0200

    Last updates

--- src/utils.py
+++ src/utils.py
@@ -83,3 +83,102 @@ def create_factor_eda(df, fig_height, graphs_per_row=3, abbreviate_names: Union[
     plt.show()
 
 
+
+def plot_numeric_relationship(data, numeric_var, categorical_var, subplot_position=(1,1,1), abbreviate_names = True):
+    """
+    Plots the relationship between a numeric variable and a categorical variable using a boxplot within a subplot.
+
+    Parameters:
+        data (pd.DataFrame): The DataFrame containing the data.
+        numeric_var (str): The name of the numeric column.
+        categorical_var (str): The name of the categorical column.
+        subplot_position (tuple): The position of the subplot (nrows, ncols, index).
+        figure_size (tuple): The overall figure size.
+    """
+    if isinstance(abbreviate_names, bool): abbvr_length = 10
+    else: abbvr_length = abbreviate_names
+    # Ensure the variable names exist in the DataFrame
+    if numeric_var not in data.columns or categorical_var not in data.columns:
+        raise ValueError("The specified columns do not exist in the DataFrame")
+
+    # Set the current subplot position
+    plt.subplot(*subplot_position)
+    
+    # Create a boxplot
+    sns.boxplot(x=categorical_var, y=numeric_var, data=data, hue=categorical_var)
+    plt.title(f'Relationship between {categorical_var} and {numeric_var}')
+    plt.xlabel(categorical_var)
+    plt.ylabel(numeric_var)
+    if abbvr_length != False:
+        xticklabels = [label.get_text()[:abbvr_length] for label in plt.gca().get_xticklabels()]
+        plt.gca().set_xticklabels(xticklabels)
+    plt.xticks(rotation=45, ha='right')
+
+
+def plot_categorical_relationship(data, target, categorical_var, subplot_position=(1,1,1), abbreviate_names: Union[bool, int] = True):
+    """
+    Plots the relationship between two categorical variables using a count plot within a subplot.
+
+    Parameters:
+        data (pd.DataFrame): The DataFrame containing the data.
+        cat_var1 (str): The name of the first categorical column.
+        cat_var2 (str): The name of the second categorical column.
+        subplot_position (tuple): The position of the subplot (nrows, ncols, index).
+        figure_size (tuple): The overall figure size.
+        abbreviate_names (Union[bool, int]): If True, abbreviate category names to 10 characters. If int, abbreviate to that many characters.
+    """
+    if isinstance(abbreviate_names, bool): abbvr_length = 10 if abbreviate_names else None
+    else: abbvr_length = abbreviate_names
+
+    # Ensure the variable names exist in the DataFrame
+    if target not in data.columns or categorical_var not in data.columns:
+        raise ValueError("The specified columns do not exist in the DataFrame")
+
+    # Set the current subplot position
+    plt.subplot(*subplot_position)
+    
+    # Create a count plot
+    sns.countplot(x=target, hue=categorical_var, data=data)
+    plt.title(f'Relationship between {target} and {categorical_var}')
+    plt.xlabel(target)
+    plt.ylabel('Count')
+
+    # Abbreviate x-tick labels if required
+    if abbvr_length != False:
+        xticklabels = [label.get_text()[:abbvr_length] for label in plt.gca().get_xticklabels()]
+        plt.gca().set_xticklabels(xticklabels)
+
+    plt.xticks(rotation=45, ha='right')
+    plt.tight_layout()
+
+
+def bivariate_w_target_variable(data, numeric_vars: bool=True, target='y', ncols=3, fig_width=15, fig_height_per_row=5):
+    """
+    Plots multiple relationships in a grid layout.
+
+    Parameters:
+        data (pd.DataFrame): The DataFrame containing the data.
+        numeric_vars (bool): If we want to plot numerical variables or categorical ones.
+    """
+    if numeric_vars:
+        cols = data.select_dtypes(include=['number']).columns
+    
+    else:
+        cols = data.select_dtypes(include=['object']).columns
+
+
+    num_plots = len(cols)
+    nrows = (num_plots + 2) // 3  # Calculate rows needed for 3 plots per row
+    ncols = 3
+
+    plt.figure(figsize=(fig_width, fig_height_per_row * nrows))  # Adjust the figure size based on the number of rows
+
+    
+    for index, col in enumerate(cols, start=1):
+        if numeric_vars: plot_numeric_relationship(data=data, numeric_var=col, categorical_var=target, subplot_position=(nrows, ncols, index))
+        else: plot_categorical_relationship(data=data, categorical_var=col, target=target, subplot_position=(nrows, ncols, index))
+
+    plt.tight_layout()
+    plt.show()
+
+
